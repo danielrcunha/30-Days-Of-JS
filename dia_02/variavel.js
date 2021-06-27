a3c95df144df47feb59c0e26c6b38e2074a84f0d
danielrcunha <74882067+danielrcunha@users.noreply.github.com>
2021-06-27 17:24:41 -0300

    commit
    commit

--- dia_02/variavel.js
+++ dia_02/variavel.js
@@ -1,6 +1,7 @@
 //01. Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
 let desafio = '30 dias de JavaScript'
 console.log('TESTE:', desafio)
+console.log('Tamanho da String: '+desafio.length)
 
     /*let endereço = 'Rua dos Pinheiros'
     console.log(`Eu moro na "${endereço}"`);    */
