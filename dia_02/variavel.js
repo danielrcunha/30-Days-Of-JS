8a144a303941c67e3a75345e0795916c62020b95
danielrcunha <74882067+danielrcunha@users.noreply.github.com>
2021-06-27 12:00:49 -0300

    commit
    commit

--- dia_02/variavel.js
+++ dia_02/variavel.js
@@ -1,3 +1,3 @@
 //01. Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
-let assign = '30 Days Of JavaScript'
-console.log(assign)
+let ntitle = '30_Days_Of_JavaScript'
+console.log(ntitle)
