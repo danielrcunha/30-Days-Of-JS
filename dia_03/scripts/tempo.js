cc37cd81125fc16e80c2b45d5f9187b5cc79b3cb
danielrcunha <74882067+danielrcunha@users.noreply.github.com>
2021-07-02 20:07:15 -0300

    commit
    commit

--- dia_03/scripts/tempo.js
+++ dia_03/scripts/tempo.js
@@ -20,6 +20,34 @@ let num1 = 10
 let num2 = '10'
 console.log(num1 === num2)
 
+//3
+let num3 = '8.9'
+let num4 = 10
+console.log(parseInt(num3) === num4)
+
+//4
+console.log(isMarried)
+console.log(10 === 10)
+console.log('a' === 'a')
+
+console.log(isMarried === false)
+console.log('10' === 10)
+console.log('A' === 'a')
+
+//5
+console.log(4 > 3) //true
+console.log(4 >= 3) //true'
+console.log(4 < 3) //false
+console.log(4 <= 3) //false
+console.log(4 == 4) //false'
+console.log(4 === 4) //true
+console.log(4 != 4) //false'
+console.log(4 !== 4) //false'
+console.log(4 != '4')//true
+console.log(4 == '4')//true'
+console.log(4 === '4')//false
+//Find the length of python and jargon and make a falsy comparison statement.
+
 
 
 
