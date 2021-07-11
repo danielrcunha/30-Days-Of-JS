1bc990322fd4d7305b4afa832530317294fce412
danielrcunha <74882067+danielrcunha@users.noreply.github.com>
2021-07-11 15:56:24 -0300

    commit
    commit

--- day_04/scripts/appCnh.js
+++ day_04/scripts/appCnh.js
@@ -123,32 +123,19 @@ else {
 */
 
 //1 - NÍVEL 3
-let dayUserInput = prompt('What day is today ?')
-let day = dayUserInput.toLowerCase()
+let recebeMes = prompt("Digite o mês:")
+let mes = recebeMes.toLowerCase()
 
-switch (day) {
-    case 'monday':
-        console.log('Today is Monday')
-        break
-    case 'tuesday':
-        console.log('Today is Tuesday')
-        break
-    case 'wednesday':
-        console.log('Today is Wednesday')
-        break
-    case 'thursday':
-        console.log('Today is Thursday')
-        break
-    case 'friday':
-        console.log('Today is Friday')
-        break
-    case 'saturday':
-        console.log('Today is Saturday')
-        break
-    case 'sunday':
-        console.log('Today is Sunday')
-        break
+switch (mes) {
+    case 'dezembro':
+        console.log("Dezembro")
+        break;
+    case 'janeiro':
+        console.log("Janeiro")
+        break;
+
     default:
-        console.log('It is not a week day.')
+        console.log("Mês invalido")
+        break;
 }
 
