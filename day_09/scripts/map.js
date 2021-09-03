1c34678058570ce3979aece8b3cc24e1b21041cf
danielrcunha <74882067+danielrcunha@users.noreply.github.com>
2021-09-02 21:50:48 -0300

    commit
    commit

---
+++ day_09/scripts/map.js
@@ -0,0 +1,34 @@
+
+
+
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya',
+]
+const countriesToUpperCase = countries.map((country) => country.toUpperCase())
+console.log(countriesToUpperCase)
+
+/*
+// Arrow function
+const countriesToUpperCase = countries.map((country) => {
+  return country.toUpperCase();
+})
+//Explicit return arrow function
+const countriesToUpperCase = countries.map(country => country.toUpperCase());
+*/
+
+
+const countriesFirstThreeLetters = countries.map((country) =>
+    country.toUpperCase().slice(0, 3)
+)
+
+
---
+++ day_09/scripts/setTimeout.js
@@ -0,0 +1,4 @@
+function sayHello() {
+    console.log('Hello')
+}
+setTimeout(sayHello, 2000)  // imprime hello depois de esperar 2 segundos.
