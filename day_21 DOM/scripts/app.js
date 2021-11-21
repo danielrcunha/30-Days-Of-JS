f4b7d5cfb94dae8bb7d2976e4040a0280ceea2cc
danielrcunha <74882067+danielrcunha@users.noreply.github.com>
2021-11-21 13:24:23 -0300

    commit
    commit

---
+++ day_21 DOM/scripts/app.js
@@ -0,0 +1,11 @@
+// syntax
+document.getElementsByTagName('tagname')
+
+const allTitles = document.getElementsByTagName('h1')
+
+console.log(allTitles) //HTMCollections
+console.log(allTitles.length) // 4
+
+for (let i = 0; i < allTitles.length; i++) {
+    console.log(allTitles[i]) // prints each elements in the HTMLCollection
+}
